99e48aaada34d9f59dc1552a051d78a540be4161
kato-arch <mimindaha_227227@yahoo.co.jp>
2020-12-09 19:44:37 +0900

    追記分、バリテーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,31 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :name, presence: true
-         validates :first_name, presence: true
-         validates :last_name, presence: true
-         validates :first_name_kana, presence: true
-         validates :last_name_kana, presence: true
-         validates :birth_day, presence: true
 
-         validates :password, presence: true, length: { minimum: 6 }
+      # 全角ひらがな、全角、カタカナ、漢字
+      FORMAT_ZEBKAKU = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
+      # 全角カタカナ
+      FORMAT_KANA = /\A[ァ-ヶー－]+\z/.freeze
+      # 英数字混合
+      FORMAT_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
 
-         
+      # パスワード６文字以上
+      validates :password, presence: true, length: { minimum: 6 }
+      # パスワード 英数字混合
+      validates_format_of :password, with: FORMAT_REGEX, message: 'パスワードには半角英数字と半角数字の両方を含めて下さい'
 
+      with_options presence: true do
+        # 名前
+        validates :name
+        # 名字
+        validates :last_name, format: { with: FORMAT_ZEBKAKU, message:'全角文字を入力して下さい' }
+        # 名前
+        validates :first_name, format: { with: FORMAT_ZEBKAKU, message:'全角文字を入力して下さい' }
+        # 名字_カナ
+        validates :last_name_kana, format: { with: FORMAT_KANA, message:'全角カタカナを入力して下さい' }
+        # 名前_カナ
+        validates :first_name_kana, format: { with: FORMAT_KANA, message:'全角カタカナを入力して下さい' }
+        # 生年月日
+        validates :birth_day
+    end
 end
