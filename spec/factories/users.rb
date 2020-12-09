bce126facbd4d7a59210f5e7b388fcb09b281985
kato-arch <mimindaha_227227@yahoo.co.jp>
2020-12-09 19:44:52 +0900

    いらないコメントアウト削除

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,16 +1,7 @@
 FactoryBot.define do
   factory :user do
-    # name {Faker::Name.last_name}
-    # name {Faker::Name.first_name}
-    # email {Faker::Internet.free_email}
-    # password {Faker::Internet.password(min_length: 6)}
-    # password_confirmation {password}
-    # first_name { person.first.kanji }
-    # last_name { person.last.kanji }
-    # first_name_kana { person.first.katakana }
-    # last_name_kana { person.last.katakana }
 
-    name                  { 'furima太郎' }
+name                      { 'furima太郎' }
     email                 { 'test@example' }
     password              { '1234abc' }
     password_confirmation { '1234abc' }
