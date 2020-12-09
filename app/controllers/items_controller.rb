e937e81d640aa0f78e4f6043d32b272f078496e4
kato-arch <mimindaha_227227@yahoo.co.jp>
2020-12-09 11:37:45 +0900

    controllerテーブル作成

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,27 @@
+class ItemsController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+
+  def destroy
+  #  @user = User.find(params[:id])
+  #  @user.destroy
+  #  redirect_to root_path
+  end
+
+
+end
