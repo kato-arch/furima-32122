aaadc44b8699098db832fe88cd2e8ae483911aa9
kato-arch <mimindaha_227227@yahoo.co.jp>
2020-12-10 19:00:49 +0900

    item(出品ページ)でのバリテーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
   belongs_to :pulldown
-end
+
+  validates :name,             presence: true
+  validates :image,            presence: true
+  validates :description,      presence: true
+  validates :price,            presence: true
+  validates :category_id,      presence: true
+  validates :condition_id,     presence: true
+  validates :fee_id,           presence: true
+  validates :region_id,        presence: true
+  validates :shipping_days_id, presence: true
+ 
+
