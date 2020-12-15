66446d9b98d94ad1848f5ea23065cf4e5884db6c
kato-arch <mimindaha_227227@yahoo.co.jp>
2020-12-15 11:33:39 +0900

    出品画面の単体テスト

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,22 +5,17 @@ RSpec.describe Item, type: :model do
 end
 
 
- describe 'ユーザー新規登録' do
+ describe '商品 新規登録 正常系' do
 
     context '新規登録 正常系' do
       it '全項目が入力されていたら、登録できる' do
       expect(@item).to be_valid
       end
+   end
 
-      it '販売価格は半角数字のみ保存可能であること' do
-      @item.price = '1234'
-      expect(@item).to  be_valid
-      end
-    end
-
-    context '新規登録 異常系' do
+    context '商品 新規登録 異常系' do
         it "商品画像を1枚つけることが必須であること" do
-          @item.image = ''
+          @item.image = nil
           @item.valid?
           expect(@item.errors.full_messages).to include "Image can't be blank"
         end
@@ -40,13 +35,13 @@ end
         it "カテゴリーの情報が必須であること" do
           @item.category_id = ''
           @item.valid?
-          expect(@item.errors.full_messages).to include "Category_id can't be blank"
+          expect(@item.errors.full_messages).to include "Category can't be blank"
         end
 
         it "商品の状態についての情報が必須であること" do
           @item.condition_id = ''
           @item.valid?
-          expect(@item.errors.full_messages).to include "Condition_id can't be blank"
+          expect(@item.errors.full_messages).to include "Condition can't be blank"
         end
 
         it "配送料の負担についての情報が必須であること" do
@@ -74,15 +69,15 @@ end
         end
 
         it "価格の範囲が、¥300~¥9,999,999の間であること" do
-          @item.price = '300<price<9999999'
+          @item.price = '200'
           @item.valid?
-          expect(@item.errors.full_messages).to include ""
+          expect(@item.errors.full_messages).to include "Price must be greater than or equal to 300"
         end
 
         it '販売価格は半角数字のみ保存可能であること' do
-          @item.condition_id = '1234a'
+          @item.price = '１２３４５'
           @item.valid?
-          expect(@item.errors.full_messages).to include ""
+          expect(@item.errors.full_messages).to include "Price is not a number"
         end
     end
   end
