a684cefd1be85c5420c955de24c537d5213d12ad
kato-arch <mimindaha_227227@yahoo.co.jp>
2020-12-15 14:43:33 +0900

    pulldownリストで「１」を選択できなようにバリデーションを設置

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,6 +13,12 @@ class Item < ApplicationRecord
   validates :fee_id,           presence: true
   validates :region_id,        presence: true
   validates :shipping_days_id, presence: true
+
+  validates :category_id,        numericality: { only_integer: 1 }
+  validates :condition_id,       numericality: { only_integer: 1 }
+  validates :fee_id,             numericality: { only_integer: 1 }
+  validates :region_id,          numericality: { only_integer: 1 }
+  validates :shipping_days_id,   numericality: { only_integer: 1 }
   
   validates :price, presence:true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}                          
   validates :price, format: {with: /\A[0-9]+\z/, message: '半角数字で入力して下さい'}
