e4c28def5861e69694a27839209113c5c9147c14
kato-arch <mimindaha_227227@yahoo.co.jp>
2020-12-15 11:32:37 +0900

    商品出品画面のバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   belongs_to :pulldown
+  has_one_attached :image
 
   validates :name,             presence: true
   validates :image,            presence: true
@@ -12,5 +13,8 @@ class Item < ApplicationRecord
   validates :fee_id,           presence: true
   validates :region_id,        presence: true
   validates :shipping_days_id, presence: true
- 
+  
+  validates :price, presence:true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}                          
+  validates :price, format: {with: /\A[0-9]+\z/, message: '半角数字で入力して下さい'}
+end
 
