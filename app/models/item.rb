4dddcaa53a0af4979b8c0c48b1ee77ada8aa5017
kato-arch <mimindaha_227227@yahoo.co.jp>
2020-12-15 15:25:56 +0900

    with_optionで記述をまとめる

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,21 +4,24 @@ class Item < ApplicationRecord
   belongs_to :pulldown
   has_one_attached :image
 
-  validates :name,             presence: true
-  validates :image,            presence: true
-  validates :description,      presence: true
-  validates :price,            presence: true
-  validates :category_id,      presence: true
-  validates :condition_id,     presence: true
-  validates :fee_id,           presence: true
-  validates :region_id,        presence: true
-  validates :shipping_days_id, presence: true
+  with_options presence: true do
+    validates :name
+    validates :image
+    validates :description
+    validates :price
+    validates :category_id
+    validates :condition_id
+    validates :fee_id           
+    validates :region_id 
+    validates :shipping_days_id
+  end
 
-  validates :category_id,        numericality: { only_integer: 1 }
-  validates :condition_id,       numericality: { only_integer: 1 }
-  validates :fee_id,             numericality: { only_integer: 1 }
-  validates :region_id,          numericality: { only_integer: 1 }
-  validates :shipping_days_id,   numericality: { only_integer: 1 }
+  with_options presence: true, numericality: { only_integer: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :region_id
+    validates :shipping_days_id
+  end
   
   validates :price, presence:true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}                          
   validates :price, format: {with: /\A[0-9]+\z/, message: '半角数字で入力して下さい'}
