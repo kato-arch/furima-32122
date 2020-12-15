1d25162192da76efb1bed2bba01d04f36d88b840
kato-arch <mimindaha_227227@yahoo.co.jp>
2020-12-15 11:37:17 +0900

    金額入力時に、販売手数料や販売利益の計算結果を表示するイベント作成

---
+++ app/javascript/item_price.js
@@ -0,0 +1,19 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const path = location.pathname
+  const pathRegex = /^(?=.*item)(?=.*edit)/
+  if (path === "/items/new" || path === "/items" || pathRegex.test(path)) {
+
+    const priceInput = document.getElementById("item-price");
+    const addTaxDom = document.getElementById("add-tax-price");
+    const profitDom = document.getElementById("profit");
+
+    priceInput.addEventListener("input", () => {
+      const inputValue = document.getElementById("item-price").value;
+      console.log(inputValue);
+        let tax = Math.floor(inputValue * 0.1)
+        console.log(tax);
+        addTaxDom.innerHTML = tax ;
+        profitDom.innerHTML = (inputValue - tax)
+    })
+  }
+});
