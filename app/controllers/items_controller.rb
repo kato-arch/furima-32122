051164921fad109f2b9aaa38ee86f426ff745347
kato-arch <mimindaha_227227@yahoo.co.jp>
2020-12-15 11:38:05 +0900

    その他、調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,28 @@
 class ItemsController < ApplicationController
+before_action :authenticate_user!, only: [:new]
 
   def index
-
+    @items = Item.all
   end
 
   def new
-
+    @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+
+    # 正常の場合、saveしてルートパスに戻る
+    if @item.save
+      redirect_to items_path
+      else
+        # 異常の場合
+      render "new"
+    end
+  end
+
+
+
 
   def show
 
@@ -18,10 +34,21 @@ class ItemsController < ApplicationController
 
 
   def destroy
-  #  @user = User.find(params[:id])
-  #  @user.destroy
-  #  redirect_to root_path
+    
   end
 
 
+end
+
+
+
+private
+
+# def user_params
+#   devise_parameter_sanitizer.permit(:deviseの処理名, keys: [:許可するキー])
+# end
+
+def item_params
+  params.require(:item).permit(:name, :description, :price, :category_id, :condition_id, :fee_id, :region_id, :shipping_days_id)
+  .merge(user_id: current_user.id)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: "items#index"
 
   
-  resources :items, only: [:new]
-
+  resources :items, only: [:new, :create]do
+ end
 end
