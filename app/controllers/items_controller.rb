77cd184c1d2e3ccfccb042e80f229afaae2d76f8
kato-arch <mimindaha_227227@yahoo.co.jp>
2020-12-16 19:22:31 +0900

    編集、更新アクションの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-before_action :authenticate_user!, only: [:new]
+before_action :authenticate_user!, only: [:new, :edit, :update]
 
 
   def index
@@ -29,7 +29,17 @@ before_action :authenticate_user!, only: [:new]
 
 
   def edit
+    @item = Item.find(params[:id])
+  end
 
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+     else
+      render "edit"
+    end
   end
 
 
