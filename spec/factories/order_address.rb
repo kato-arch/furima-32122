64972da528acbf792f7903e258c7fc391f5de8b5
kato-arch <mimindaha_227227@yahoo.co.jp>
2020-12-21 18:52:29 +0900

    Form_object→テストコード用、ファクトリー作成

---
+++ spec/factories/order_address.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :order_address do
+
+    postal_code { '123-4567' }
+    region_id { 2 }
+    city { '横浜市' }
+    address_line { '青葉区' }
+    building_number { 'ビル名' }
+    phone_number { '09012345678' }
+    token { 'tok_abcdefghijk00000000000000000' }
+
+  end
+end 
