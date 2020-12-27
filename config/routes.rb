7506a951678c5139d061e207aba095757480adcf
kato-arch <mimindaha_227227@yahoo.co.jp>
2020-12-27 15:14:19 +0900

    12月27日(日)　コメント学習

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+  # ルートパスの設定
   root to: "items#index"
-
+  # usersのパス
   resources :users, only: :show
 
-    # itemsに関しては7つのアクション全て入っているので略している
+  # itemsのパス
+  # itemsに関しては7つのアクション全て入っているので略している
   resources :items do
     # ordersはitemsのなかに紐付いている（do~endの中に入っている）
     resources :orders, only: [:index, :create]
